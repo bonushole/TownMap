7010dfb5b9add5fb93a751d83c93be596c30bede
bonushole <156max@gmail.com>
2016-12-17 21:20:32 +0000

    added php files for accepting and declining requests

---
+++ php/declineRequest.php
@@ -0,0 +1,20 @@
+ <?php
+
+$servername = "max000000.db.10570502.hostedresource.com";
+$username = "max000000";
+$password = "Blue78TigerMarble%";
+$dbname = "max000000";
+    
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    
+    die("Connection failed: " . $conn->connect_error);
+    echo json_encode("something went wrong");
+} 
+
+$sql ="DELETE FROM requests WHERE id="+$_GET['id']+";";
+
+$result = $conn->query($sql);
+$conn->close();
+
+?>
