d116400eb25f0c2e7f90d6989cbb8f4f42460a71
bonushole <156max@gmail.com>
2016-12-17 17:08:56 +0000

    updated database login everywhere

--- php/resolveHouse.php
+++ php/resolveHouse.php
@@ -1,9 +1,11 @@
 <?php
 
-$servername = "localhost";
-$username = "user";
-$password = "password";
-$dbname = "snowplow";
+
+$servername = "max000000.db.10570502.hostedresource.com";
+$username = "max000000";
+$password = "Blue78TigerMarble%";
+$dbname = "max000000";
+    
     
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
