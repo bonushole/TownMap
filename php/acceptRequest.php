d7a7c1967cc96b5ed763fb086af4e1768afe2f0e
bonushole <156max@gmail.com>
2016-12-17 23:45:41 +0000

    added admin ability to accept a request

--- php/acceptRequest.php
+++ php/acceptRequest.php
@@ -1,1 +1,26 @@
- 
+<?php
+
+$servername = "max000000.db.10570502.hostedresource.com";
+$username = "max000000";
+$password = "Blue78TigerMarble%";
+$dbname = "max000000";
+    
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    
+    die("Connection failed: " . $conn->connect_error);
+    echo json_encode("something went wrong");
+} 
+
+$sql ="SELECT * FROM requests WHERE id=". $_GET['id'] .";";
+
+$result = $conn->query($sql);
+$row = $result->fetch_assoc()
+
+$sql ="DELETE FROM requests WHERE id=". $_GET['id'] .";";
+$conn->query($sql);
+
+$sql ="INSERT INTO houses VALUES(0,".$row.address.",1)";
+$conn->close();
+
+?>
