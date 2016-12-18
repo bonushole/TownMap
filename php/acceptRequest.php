2c8b450fbefa747f7826d564d0d4e7afe42f449a
bonushole <156max@gmail.com>
2016-12-18 02:00:40 +0000

    finished accept

--- php/acceptRequest.php
+++ php/acceptRequest.php
@@ -15,12 +15,14 @@ if ($conn->connect_error) {
 $sql ="SELECT * FROM requests WHERE id=". $_GET['id'] .";";
 
 $result = $conn->query($sql);
-$row = $result->fetch_assoc()
+$row = $result->fetch_assoc();
 
 $sql ="DELETE FROM requests WHERE id=". $_GET['id'] .";";
-$conn->query($sql);
+$result2 =$conn->query($sql);
 
-$sql ="INSERT INTO houses VALUES(0,".$row.address.",1)";
+//$sql ="INSERT INTO houses VALUES(0,". $row['address'] .",1)";
+$sql ='INSERT INTO houses VALUES (0,"'.$row['address'].'", true);';
+$result3=$conn->query($sql);
 $conn->close();
 
 ?>
